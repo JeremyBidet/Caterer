f058cdd623137e94211ff588089856c73856418a
WhyTSwag <jeremy.bidet@gmail.com>
2014-05-29 16:07:39 +0200

    First commit
    Add arch.

---
+++ src/fr/upem/jbidet/caterer/Main.java
@@ -0,0 +1,14 @@
+package fr.upem.jbidet.caterer;
+
+/**
+ * @author dody
+ * @version 1.0
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
